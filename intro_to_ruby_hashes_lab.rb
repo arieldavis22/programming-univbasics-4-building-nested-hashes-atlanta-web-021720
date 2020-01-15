3e1dd547ab73472f276e0121ecca783edda8eb0a
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-15 16:18:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	test_hash = {
-		:railroads => {
-			:pieces => 4
-		}
+		:railroads => {pieces: 4, rent_in_dollars: "apple"}
 	}
 end
 
